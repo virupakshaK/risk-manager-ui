e448de5bc871911034f9a427f55aa9915bdb28b3
Virupaksha <virupaksha579@gmail.com>
2024-08-23 23:31:25 +0530

    Drawer impletation

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Layout from './risk-manager/components/Layout.js'
 import Header from './risk-manager/components/Header.js'
 import MainPage from './risk-manager/components/MainPage.js';
 import Test from './risk-manager/components/Test.js';
+import LeftDrawerMenu from './risk-manager/components/access-management/LeftDrawerMenu.js';
 //import Login from './risk-manager/Login.js';
 const App = () => {
   return (
     <div className='App'>
 
    
-         <MainPage />
+         <LeftDrawerMenu />
 
          
     </div>
